fc550e88c1158771dbb5eb0e2abe3035bf561077
Dario Sanchez <dariofs1994@gmail.com>
2025-09-15 11:43:45 -0300

    constants

---
+++ contracts/lance/src/storage/constants.rs
@@ -0,0 +1,13 @@
+/********** Numbers **********/
+
+/// Fixed-point scalar for 12 decimal numbers
+pub const SCALAR_12: i128 = 1_000_000_000_000;
+
+/// Fixed-point scalar for 7 decimal numbers
+pub const SCALAR_7: i128 = 1_0000000;
+
+/// Seconds per year
+pub const SECONDS_PER_MONTH: i128 = 2628000;
+
+/// Seconds per week
+pub const SECONDS_PER_WEEK: u64 = 604800;
--- contracts/lance/src/storage/mod.rs
+++ contracts/lance/src/storage/mod.rs
@@ -5,3 +5,4 @@ pub mod storage;
 pub mod user;
 pub mod dispute_status;
 pub mod service_status;
+pub mod vote;

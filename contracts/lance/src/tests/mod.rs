4ebe9c00c1e45ed2eddf8134187c1c10575f9f2b
masch <the.masch@gmail.com>
2025-09-15 18:45:02 -0300

    Add user test

--- contracts/lance/src/tests/mod.rs
+++ contracts/lance/src/tests/mod.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod milestone;
 pub mod service;
+pub mod user;
--- contracts/lance/src/tests/user.rs
+++ contracts/lance/src/tests/user.rs
@@ -1,33 +1,37 @@
 #![cfg(test)]
 
-use soroban_sdk::{testutils::Address as _, Address, Env, String};
+use crate::{
+    storage::service_status::ServiceStatus,
+    tests::config::{constants::BASE_MINT_AMOUNT, contract::ContractTest},
+};
+
+use soroban_sdk::{
+    testutils::{Address as _, Ledger as _, MockAuth, MockAuthInvoke},
+    token, Address, Env, String,
+};
 
 use crate::contract::{Contract, ContractClient};
-
 #[test]
 fn test_add_and_get_user() {
-    let env = Env::default();
-    env.mock_all_auths();
-
-    let admin = Address::generate(&env);
-    let token_admin = Address::generate(&env);
-
-    let (client, token, token_admin_client, token_id) = create_contract(&env, &admin, &token_admin);
-
-    let user_1 = Address::generate(&env);
+    let ContractTest {
+        env,
+        contract,
+        employee_1,
+        ..
+    } = ContractTest::setup();
 
     let personal_data = String::from_str(&env, "Employee 1");
-    client.new_user(
-        &user_1,
+    contract.new_user(
+        &employee_1,
         &true,  // is_employee
         &false, // is_employer
         &false, // is_judge
         &Some(personal_data.clone()),
     );
 
-    let user_data = client.get_user(&user_1);
+    let user_data = contract.get_user(&employee_1);
 
-    assert_eq!(user_data.address, user_1);
+    assert_eq!(user_data.address, employee_1);
     assert_eq!(user_data.is_employee, true);
     assert_eq!(user_data.is_employer, false);
     assert_eq!(user_data.is_judge, false);
